8f413cd212ca2dec28b14498780d348f6ca12acd
comradeos <57126875+comradeos@users.noreply.github.com>
2020-03-26 12:54:31 +0200

    Update index.php

--- php/index.php
+++ php/index.php
@@ -2,11 +2,11 @@
 <html>
   <head>
     <meta charset="utf-8"/>
-    <title>версия-1</title>
+    <title>версия-2</title>
   </head>
   <body>
     <?
-      echo "<h1>версия-1</h1>";
+      echo "<h1>версия-2</h1>";
       //в мастер
     ?>
   </body>
