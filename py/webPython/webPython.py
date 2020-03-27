2a7c1f584ca15179652110b8eba75547a4b24695
comradeos <57126875+comradeos@users.noreply.github.com>
2020-03-27 14:01:11 +0200

    Create webPython.py

---
+++ py/webPython/webPython.py
@@ -0,0 +1,18 @@
+#!C:\Python\python.exe
+
+
+print ("Content-Type: text/html\r\n")
+print ("<html><head><title>тестим веб-питон</title></head><body>")
+print ("<h1>тестим веб-питон</h1>")
+print ("<p>параграф</p><h1>- * -</h1>")
+
+c = 99
+
+def test():
+    s = 12
+    a = s ** 2
+    print("Результат", a, c)
+
+test()
+
+print ("</body></html>")
