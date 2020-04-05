1d2b2fc6827741037458fe5d04e93c18ca7a3ed7
Iaroslav Os <osmnbx@gmail.com>
2020-04-05 22:42:55 +0300

    переименовал файл

---
+++ php/helloPhp/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>версия-3</title>
+  </head>
+  <body>
+    <?
+      echo "<h1>версия-3 обновлено</h1>";
+      //в новую ветку !
+    ?>
+  </body>
+</html>
