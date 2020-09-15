9b6b0452c33aaea61728d758b25cb3b9774f3aa3
comradeos <57126875+comradeos@users.noreply.github.com>
2020-09-15 15:20:22 +0300

    Update main.py

--- python/num_ceil_floor/main.py
+++ python/num_ceil_floor/main.py
@@ -1,15 +1,15 @@
 # num ceil floor
 
-def my_ceil(num):  # в большую сторону
-    return int(-(-num // 1))
+def my_ceil(n):  # в большую сторону
+    return int(-(-n // 1))
 
 
-def my_floor(num):  # в меньшую сторону
-    return int(num - num % 1)
+def my_floor(n):  # в меньшую сторону
+    return int(n - n % 1)
 
 
-num = float(input("введите дробное число: "))
+number = float(input("введите дробное число: "))
 
 
-print("округление в большую сторону: ", my_ceil(num))
-print("округление в меньшую сторону: ", my_floor(num))
+print("округление в большую сторону: ", my_ceil(number))
+print("округление в меньшую сторону: ", my_floor(number))
