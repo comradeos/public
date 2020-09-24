c959ded4e5fdecb6a5a0b30ca4c0461574e2a202
comradeos <57126875+comradeos@users.noreply.github.com>
2020-09-24 10:38:57 +0300

    Update main.py

--- python/kh_lects/main.py
+++ python/kh_lects/main.py
@@ -1,17 +1,34 @@
-import time
+# -*- coding: utf-8 -*-
 
-time.sleep(0)
+def try_input():
+    time = 5
+    for i in range(0, time, 1):
+        import time
+        time.sleep(1)
+        print(".", end="")
+    print()
+    while True:
+        base = input()
+        try:
+            base = int(base)
+            print("<<<", base)
+        except ValueError:
+            if base == "stop":
+                exit()
+            print("не число")
 
-while True:
-    base = input()
-    try:
-        base = int(base)
-        print("<<<", base)
-    except ValueError:
-        if base == "stop":
-            exit()
-        print("не число")
+
+# try_input()
+
+def file_rw():
+    file = open("file.txt", "a", encoding="utf-8")
+    file.write("ЫЫЫЫЭ")
+
+    file = open("file.txt", "r", encoding="utf-8")
+    result = file.read()
+    print(">", result)
+
+    file.close()
 
 
-
-
+file_rw()
