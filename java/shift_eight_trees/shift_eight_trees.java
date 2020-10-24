e61293bbf1b20a0a885a030e281db22a3f5cd558
comradeos <57126875+comradeos@users.noreply.github.com>
2020-10-24 12:05:29 +0300

    Create shift_eight_trees.java

---
+++ java/shift_eight_trees/shift_eight_trees.java
@@ -0,0 +1,32 @@
+public class shift_eight_trees {
+    public static void main(String[] args) {
+        System.out.println("shift_eight_trees v.1.0:\n");
+
+        int size = 27;
+
+        for (int i = size; i >= 1; i--) {
+            for (int j = 0; j <= size; j++ ) {
+                if (j >= i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i <= size; i++) {
+            for (int j = 0; j <= size; j++ ) {
+                if (j >= i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+    }
+}
