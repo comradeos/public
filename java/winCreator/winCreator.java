1f968c165be7ce2f858180fd8530ab2c1428a182
comradeos <57126875+comradeos@users.noreply.github.com>
2020-11-05 09:50:30 +0200

    Update and rename win_creator.java to winCreator.java

--- java/winCreator/winCreator.java
+++ java/winCreator/winCreator.java
@@ -87,7 +87,7 @@ class application {
 
 }
 
-public class win_creator {
+public class winCreator {
     public static void main(String[] args) {
         application application = new application();
         application.window();
