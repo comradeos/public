cf35631ee06f910d5f00778c3c690b0b51150c04
comradeos <57126875+comradeos@users.noreply.github.com>
2021-03-09 11:49:51 +0200

    Update index.php

--- jscript/using-cookies/index.php
+++ jscript/using-cookies/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
-        <meta charset="utf-8">
-        <title>cookies test</title>
+        <meta charset="utf-8"/>
+        <title>cookie test</title>
     </head>
     <body>
         <script src="script.js"></script>
