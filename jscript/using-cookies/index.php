dc542654963adc215ce399866bc3eceb8c015ee1
comradeos <57126875+comradeos@users.noreply.github.com>
2021-03-09 11:47:37 +0200

    Add files via upload

---
+++ jscript/using-cookies/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>cookie test</title>
+    </head>
+    <body>
+        <script src="script.js"></script>
+
+    </body>
+</html>
