a332d215986578e926e1d10f3829b8d32b4a9be8
comradeos <57126875+comradeos@users.noreply.github.com>
2021-03-23 14:44:42 +0200

    Add files via upload

---
+++ python/эрик_мэтиз_изучаем_python_2020/src/exercises_160.py
@@ -0,0 +1,28 @@
+print("8.9")
+messages = ['hi', 'hello', 'what\'s up']
+sent_messages = []
+
+
+def show_messages():
+    for message in messages:
+        print(message)
+
+
+print("\n8.10")
+
+
+def send_messages():
+    while messages:
+        current_message = messages.pop()
+        print(f"sending '{current_message}'... ", end="")
+        sent_messages.append(current_message)
+        print(f" ... message sent")
+
+
+show_messages()
+send_messages()
+
+print("\n8.11")
+
+print(f"messages::{messages}")
+print(f"sent_messages::{sent_messages}")
