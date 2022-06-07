9343cdca8d039ee67d7cc427007445feb3dc5cd7
Iaroslav Os <osmnbx@gmail.com>
2022-06-07 20:29:14 +0300

    start over

--- parking/index.php
+++ parking/index.php
@@ -1,4 +1,9 @@
-<?php require_once __DIR__ . '/core.php'; ?>
+<?php
+
+require_once __DIR__ . '/session.php';
+require_once __DIR__ . '/core.php';
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,75 +17,9 @@
 </head>
 
 <body>
-
-    <div class="container">
-
-        <div class="alarm">
-            <form action="act.php" method="post">
-                <input type="submit" value="Alarm">
-            </form>
-        </div>
-
-        <h1>Parking_v2_1_web_01</h1>
-        <h2>State:</h2>
-
-        <form method="GET">
-            <button name="top1" class="<?php print $set_in['param1']; ?>">top1 (param1)</button><br>
-            <div class="car-space">car-space</div>
-            <button name="bottom1" class="<?php print $set_in['param2']; ?>">bottom1 (param2)</button><br>
-            <button name="bottom2" class="<?php print $set_in['param3']; ?>">bottom2 (param3)</button>
-        </form>
-
-        <div class="space1"></div>
-
-        <h2>Settings:</h2>
-
-        <form method="POST">
-
-            <table>
-                <tr>
-                    <th>Parameter</th>
-                    <th>Value</th>
-                    <th>Units</th>
-                </tr>
-
-                <tr>
-                    <td><label>param1</label></td>
-                    <td><input type="text" name="param1" value="<?php print $set_in['param1']; ?>"></td>
-                    <td><label>color.</label></td>
-                </tr>
-
-                <tr>
-                    <td><label>param2</label></td>
-                    <td><input type="text" name="param2" value="<?php print $set_in['param2']; ?>"></td>
-                    <td><label>color.</label></td>
-                </tr>
-
-                <tr>
-                    <td><label>param3</label></td>
-                    <td><input type="text" name="param3" value="<?php print $set_in['param3']; ?>"></td>
-                    <td><label>color.</label></td>
-                </tr>
-
-                <tr>
-                    <td><label>param4</label></td>
-                    <td><input type="text" name="param4" value="<?php print $set_in['param4']; ?>"></td>
-                    <td><label>unit.</label></td>
-                </tr>
-
-                <tr>
-                    <td><label>param5</label></td>
-                    <td><input type="text" name="param5" value="<?php print $set_in['param5']; ?>"></td>
-                    <td><label>unit.</label></td>
-                </tr>
-
-            </table>
+    <div>Welcome, <?php echo $_SESSION['user']; ?> | <a href="logout.php">Sign out</a></div>
 
 
-            <input type="submit" class="save" value="Save">
-        </form>
-
-    </div>
 </body>
 
 </html>
