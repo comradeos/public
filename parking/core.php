5eb6b65680b2330e1bd0de40ed20b5cd4c566876
Iaroslav Os <osmnbx@gmail.com>
2022-06-07 20:28:08 +0300

    updated logic v2

--- parking/core.php
+++ parking/core.php
@@ -2,46 +2,16 @@
 
 require_once __DIR__ . '/tools.php';
 
-// проверка метода отправки данных
+$ini = parse_ini_file('set_in.ini', true);
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $ini = parse_ini_file('set_in.ini', true);
 
-    // строка для хранения новых настроек
-    $new_set = '';
-    // получение данных из формы
-    $set_out = [
-        'param1' => $_POST['param1'] ?? null,
-        'param2' => $_POST['param2'] ?? null,
-        'param3' => $_POST['param3'] ?? null,
-        'param4' => $_POST['param4'] ?? null,
-        'param5' => $_POST['param5'] ?? null,
-        'alarm' => $ini['alarm'],
-    ];
-
-    // перебор массива с формы с присваиванием в строку новых настроек
-    foreach ($set_out as $key => $value) {
-        $new_set .= "$key=$value\n";
-    }
-
-    // получение текущей даты/времени
     $date = new DateTime();
-    // форматирование даты
     $now = $date->format('Y-m-d H:i:s');
-
-    // запись даты в настройки
     $new_set .= "mod_date=$now";
-    __print($new_set);
 
-    // запись в файлы
-    file_put_contents('set_in.ini', $new_set);
+    // file_put_contents('set_in.ini', $new_set);
     file_put_contents('set_out.ini', $new_set);
 }
 
-$alarm = $_GET['alarm'] ?? '';
-if ($alarm != '') {
-    echo 1111111111;
-}
-
 $set_in = parse_ini_file('set_in.ini', true);
-// __print($set_in);
-// __print(array_keys($set_in)[0]);
