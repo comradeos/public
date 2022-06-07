fd5f10ee177eced532b5f1fcb0cea1fa217cee65
Iaroslav Os <osmnbx@gmail.com>
2022-06-08 00:03:25 +0300

    updated read/write ini logic

--- parking/core.php
+++ parking/core.php
@@ -3,7 +3,16 @@
 require_once __DIR__ . '/tools.php';
 
 $ini = parse_ini_file('set_in.ini', true);
-__print($ini);
+
+/* -> css classes */
+$cam1 = $ini['camera_1'] == 1 ? 'on' : 'off';
+$cam2 = $ini['camera_2'] == 1 ? 'on' : 'off';
+$gate = $ini['gate'] == 1 ? 'on' : 'off';
+$car_moving = $ini['gate'] == 1 ? 'car_moving' : '';
+$alarm = $ini['alarm'] == 1 ? 'alarm_on' : 'alarm_off';
+/* <- css classes */
+
+// __print($ini);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $updates = '';
@@ -20,18 +29,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if (isset($_POST['reset_timer'])) {
         $config['reset_timer'] = $_POST['reset_timer'];
-    }
+    } // хз шо тут
 
     if (isset($_POST['reset_timer'])) {
         $config['alarm_timer'] = $_POST['alarm_timer'];
-    }
+    } // хз шо тут
 
     if (isset($_POST['alarm'])) {
-        $config['alarm'] = ($config['alarm'] == 0) ? 1 : 0;
+        $config['alarm'] = $config['alarm'] == 0 ? 1 : 0;
     }
 
     if (isset($_POST['gate'])) {
-        $config['gate'] = ($config['gate'] == 0) ? 1 : 0;
+        $config['gate'] = $config['gate'] == 0 ? 1 : 0;
+    }
+
+    if (isset($_POST['camera_1'])) {
+        $config['camera_1'] = $config['camera_1'] == 0 ? 1 : 0;
+    }
+
+    if (isset($_POST['camera_2'])) {
+        $config['camera_2'] = $config['camera_2'] == 0 ? 1 : 0;
     }
 
     __print($config);
@@ -42,4 +59,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     file_put_contents('set_out.ini', $updates);
     file_put_contents('set_in.ini', $updates);
+    header('Location: index.php');
 }
