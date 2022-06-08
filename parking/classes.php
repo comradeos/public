d500d331ffbf36d795e7289f5603cd3c2b9ccc90
Iaroslav Os <osmnbx@gmail.com>
2022-06-09 00:04:55 +0300

    upd classes format

--- parking/classes.php
+++ parking/classes.php
@@ -3,10 +3,13 @@
 $ini = parse_ini_file('set_in.ini', true);
 
 /* -> css classes */
+
 $cam1 = $ini['camera_1'] == 1 ? 'on' : 'off';
 $cam2 = $ini['camera_2'] == 1 ? 'on' : 'off';
 $gate = $ini['gate'] == 1 ? 'on' : 'off';
+$gate_oc = $ini['gate'] == 1 ? 'Opened ' : 'Closed ';
 $car_moving = $ini['gate'] == 1 ? 'car_moving' : '';
 $car = $ini['car'] == 1 ? 'car_in' : 'car_out';
 $alarm = $ini['alarm'] == 1 ? 'alarm_on' : 'alarm_off';
+
 /* <- css classes */
