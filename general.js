a62f01408f287da8cc62968787a8aae32ef03c71
Iaroslav Os <osmnbx@gmail.com>
2022-07-08 16:18:16 +0300

    updated content

--- general.js
+++
@@ -1,9 +0,0 @@
-function readFile(file) {
-    fetch(file)
-        .then(response => response.text())
-        .then(textString => {
-            document.getElementById('header').innerHTML = textString;
-        });
-}
-
-// readFile('./test.html');
